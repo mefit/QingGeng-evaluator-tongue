31ce2c2bcd3c2666a2e8a5333e5c782a1d9c0677
Aidi Stan <aidistan@live.com>
2018-11-28 11:39:31 +0800

    Upload images to QingGeng

--- tongue_tasks.py
+++ tongue_tasks.py
@@ -2,6 +2,7 @@ from celery import Celery
 import tempfile
 import requests
 import tongue_analysis
+from PIL import Image
 import json
 
 app = Celery(__name__)
@@ -17,11 +18,16 @@ def evaluate(user_id, id, url):
         with tongue_analysis.setup('./tongue.pb') as sess:
             result = tongue_analysis.analyze(f.name, sess = sess)
 
+    with tempfile.NamedTemporaryFile() as f_m, tempfile.NamedTemporaryFile() as f_t:
         content = {'舌质':result['shezhi'], '舌苔':result['shetai']}
+
+        Image.fromarray(result['mosaic_img']).save(f_m.name, 'jpeg')
+        Image.fromarray(result['tongue_img']).save(f_t.name, 'jpeg')
+        files = [
+            ('files[]', ('mosaic', open(f_m.name, 'rb'), 'image/jpeg')),
+            ('files[]', ('tongue', open(f_t.name, 'rb'), 'image/jpeg'))]
+
         requests.put(
             'https://qinggeng.app.aidistan.site/api/users/%s/evaluations/%s' % (user_id, id),
-            headers = {'Authorization':'JCMXSorcbAAs8iVobTFGBxhX'},
+            headers = {'Authorization':'JCMXSorcbAAs8iVobTFGBxhX'}, files = files,
             data = {'completed':True, 'kind':'JSON', 'content':json.dumps(content)})
-
-        #from PIL import Image
-        #Image.fromarray(result['mosaic_img']).save('mosaic.jpg', 'jpeg')
