0e71577042969d880461a44e39787846ebb10551
Aidi Stan <aidistan@live.com>
2018-11-28 10:29:07 +0800

    Use requests to update

--- tongue.py
+++ tongue.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/", methods = ["GET", "POST"])
 def listen():
     evaluate.delay(
-        request.form['id'],
         request.form['user_id'],
+        request.form['id'],
         request.form['tongue_image'])
     return "ok", 200
--- tongue_tasks.py
+++ tongue_tasks.py
@@ -1,19 +1,27 @@
 from celery import Celery
 import tempfile
-import urllib.request
+import requests
 import tongue_analysis
+import json
 
 app = Celery(__name__)
 app.conf.update(broker_url = 'redis://localhost/1')
 
 @app.task
-def evaluate(id, user_id, url):
-    with tempfile.NamedTemporaryFile() as tf:
-        with urllib.request.urlopen(url) as f:
-            tf.write(f.read())
+def evaluate(user_id, id, url):
+    with tempfile.NamedTemporaryFile() as f:
+        with requests.get(url, stream = True) as r:
+            for chunk in r.iter_content(chunk_size = 128):
+                f.write(chunk)
 
         with tongue_analysis.setup('./tongue.pb') as sess:
-            result = tongue_analysis.analyze(tf.name, sess = sess)
+            result = tongue_analysis.analyze(f.name, sess = sess)
 
-        print(result)
-        # JCMXSorcbAAs8iVobTFGBxhX
+        content = {'舌质':result['shezhi'], '舌苔':result['shetai']}
+        requests.put(
+            'https://qinggeng.app.aidistan.site/api/users/%s/evaluations/%s' % (user_id, id),
+            headers = {'Authorization':'JCMXSorcbAAs8iVobTFGBxhX'},
+            data = {'completed':True, 'kind':'JSON', 'content':json.dumps(content)})
+
+        #from PIL import Image
+        #Image.fromarray(result['mosaic_img']).save('mosaic.jpg', 'jpeg')
