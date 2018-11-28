94c81abc40de220744792501cc802fcfdcb670e8
Aidi Stan <aidistan@live.com>
2018-11-28 11:36:57 +0800

    Improve run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
+tmux \
+  has-session -t evaluator-tongue \; \
+  kill-session -t evaluator-tongue \;
+
 if [ "$1" = "dev" ]; then
   tmux \
-    kill-session -t evaluator-tongue \; \
     new-session 'FLASK_ENV=development FLASK_APP=tongue pipenv run flask run' \; \
     split-window 'pipenv run celery -A tongue_tasks worker --loglevel=debug' \; \
     rename-session 'evaluator-tongue-devel' \;
 else
   tmux \
-    kill-session -t evaluator-tongue \; \
     new-session 'FLASK_APP=tongue pipenv run flask run' \; \
     split-window 'pipenv run celery -A tongue_tasks worker' \; \
     rename-session 'evaluator-tongue' \;
